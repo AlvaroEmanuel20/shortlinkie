9a5055ba3a4983067099ca112ff6cd225f780470
Álvaro Emanuel <alvarodiasribeiro16@gmail.com>
2024-01-24 10:11:00 -0300

    feat(13): Add link search feature to app

--- frontend/src/components/dashboard.components/AppBar.tsx
+++ frontend/src/components/dashboard.components/AppBar.tsx
@@ -9,9 +9,16 @@ import { AuthContext } from '../../context/auth.context/AuthContext';
 import MobileSidebar from './MobileSidebar';
 import { Grip } from 'lucide-react';
 import { Stack } from '../Stack';
+import useQuery from '../../hooks/useQuery';
+import { ShortUrl } from '../../lib/types';
+import { toast } from 'react-toastify';
 
 const AppbarStyle = styled.header`
   margin-bottom: 50px;
+
+  .search-box {
+    position: relative;
+  }
 
   .search {
     width: 380px;
@@ -39,8 +46,37 @@ const AppbarStyle = styled.header`
     margin-bottom: 40px;
 
     .search,
+    .search-box,
     .user {
       display: none;
+    }
+  }
+`;
+
+const SearchResultsContainer = styled.div`
+  position: absolute;
+  top: 3rem;
+  left: 0;
+  background-color: ${(props) => props.theme.colors.light};
+  border-radius: ${(props) => props.theme.radius.base};
+  width: 100%;
+  padding: 10px;
+  box-shadow: 4px 6px 10px -5px rgba(0, 0, 0, 0.1);
+  min-height: 50px;
+
+  .search-result {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+    li {
+      padding: 5px;
+      border-radius: ${(props) => props.theme.radius.base};
+    }
+
+    li:hover {
+      background-color: ${(props) => props.theme.colors.gray1};
     }
   }
 `;
@@ -50,6 +86,21 @@ export default function Appbar() {
   const [mobileSidebar, setMobileSidebar] = useState(false);
   const theme = useTheme();
 
+  const [search, setSearch] = useState('');
+  const [showResults, setShowResults] = useState(false);
+
+  const { data: shorturls, isLoading: isLoadingShorturls } = useQuery<
+    ShortUrl[]
+  >('/shorturls', (error) => {
+    if (error) toast.error('Erro ao carregar seus links.');
+  });
+
+  const shorturlsResult = shorturls
+    ? shorturls.filter((value) =>
+        value.title.toLowerCase().startsWith(search.toLowerCase())
+      )
+    : [];
+
   return (
     <>
       <AppbarStyle>
@@ -58,12 +109,38 @@ export default function Appbar() {
             <img width={40} src="/logo-blue.svg" alt="Logo do Encurtando" />
           </Link>
 
-          <Input
-            size="small"
-            type="search"
-            placeholder="Pesquisar"
-            className="search"
-          />
+          <div className="search-box">
+            <Skeleton isLoading={isLoadingShorturls}>
+              <Input
+                size="small"
+                type="search"
+                placeholder="Pesquisar"
+                className="search"
+                onFocus={() => setShowResults(true)}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </Skeleton>
+
+            {showResults && search.length > 0 && (
+              <SearchResultsContainer>
+                {shorturlsResult.length > 0 ? (
+                  <ul className="search-result">
+                    {shorturlsResult.map((value) => (
+                      <Link
+                        key={value.shortId}
+                        onClick={() => setSearch('')}
+                        to={`/link/${value.shortId}`}
+                      >
+                        <li>{value.title}</li>
+                      </Link>
+                    ))}
+                  </ul>
+                ) : (
+                  'Nenhum resultado'
+                )}
+              </SearchResultsContainer>
+            )}
+          </div>
 
           <Queue className="user" spacing={10}>
             <Stack spacing={2} $items="flex-end" className="user-info">
