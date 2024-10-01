743f32a3f7ace2b6bd07c76b46b8dd6b192f4c14
Álvaro Emanuel <90700097+AlvaroEmanuel20@users.noreply.github.com>
2024-09-30 23:33:16 -0300

    change: delete frontend and transform in API only

--- frontend/src/components/InputError.ts
+++
@@ -1,6 +0,0 @@
-import { styled } from 'styled-components';
-
-export const InputError = styled.p`
-  font-size: ${(props) => props.theme.fontSize.small};
-  color: ${(props) => props.theme.colors.red};
-`;
--- frontend/src/components/dashboard.components/ChartWrapper.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ChartWrapper = styled.div<{ height?: number }>`
-  height: ${(props) => (props.height ? `${props.height}px` : '55px')};
-  width: 100%;
-`;
